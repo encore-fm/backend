a43bebcc74ee96ba823ea808171dc21f83aa0900
Anton Baumann <antonbaumann4@gmail.com>
2020-03-18 15:26:36 +0100

    getUser and addUser functions

--- backend/handlers/userHandler.go
+++ backend/handlers/userHandler.go
@@ -2,10 +2,10 @@ package handlers
 
 import (
 	"context"
+	"errors"
 	"net/http"
 
 	"github.com/antonbaumann/spotify-jukebox/user"
-	"github.com/davecgh/go-spew/spew"
 	"github.com/gorilla/mux"
 	log "github.com/sirupsen/logrus"
 	"go.mongodb.org/mongo-driver/bson"
@@ -16,11 +16,29 @@ type UserHandler struct {
 	UserCollection *mongo.Collection
 }
 
-func (h *UserHandler) addUser(user *user.Model) error {
-	filter := bson.D{{"username", user.Username}}
-	result := h.UserCollection.FindOne(context.TODO(), filter)
-	spew.Dump(result)
-	return nil
+var (
+	ErrUsernameTaken = errors.New("requested username already taken")
+)
+
+func (h *UserHandler) getUser(username string) (*user.Model, error) {
+	filter := bson.D{{"username", username}}
+	var foundUser *user.Model
+	err := h.UserCollection.FindOne(context.TODO(), filter).Decode(&foundUser)
+	// if username does not exist -> no documents in result error
+	if err != nil {
+		return nil, err
+	}
+	return foundUser, nil
+}
+
+func (h *UserHandler) addUser(newUser *user.Model) error {
+	_, err := h.getUser(newUser.Username)
+	if err != mongo.ErrNoDocuments {
+		return ErrUsernameTaken
+	}
+
+	_, err = h.UserCollection.InsertOne(context.TODO(), newUser)
+	return err
 }
 
 func (h *UserHandler) Join(w http.ResponseWriter, r *http.Request) {
@@ -29,8 +47,11 @@ func (h *UserHandler) Join(w http.ResponseWriter, r *http.Request) {
 	newUser := user.New(vars["username"])
 	if err := h.addUser(newUser); err != nil {
 		log.Infof("user [%v] tried to join but username is already taken", newUser.Username)
+		w.WriteHeader(http.StatusConflict)
+		return
 	}
 
+	log.Infof("user [%v] joined", newUser.Username)
 	w.WriteHeader(http.StatusOK)
-	log.Println("/user/join")
+	// todo: send back user secret and store in database
 }
