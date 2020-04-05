a46729c95f3eb280a3522feb6ae24067142b9730
omareldeeb <omareldeeb00@live.com>
2020-04-05 22:52:20 +0200

    Implement system tests

---
+++ systest/user_auth_token_test.go
@@ -0,0 +1,50 @@
+package systest
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/antonbaumann/spotify-jukebox/handlers"
+	"github.com/antonbaumann/spotify-jukebox/user"
+	"github.com/stretchr/testify/assert"
+	"go.mongodb.org/mongo-driver/bson"
+	"golang.org/x/oauth2"
+	"testing"
+)
+
+func Test_UserGetAuthToken_Authorized(t *testing.T) {
+	dropDB()
+	setupDB()
+
+	var usr user.Model
+	err := userCollection.FindOne(
+		context.Background(),
+		bson.D{{"_id", testAdmin.ID}}).
+		Decode(&usr)
+	// refresh token not expected
+	usr.AuthToken.RefreshToken = ""
+
+	resp, err := UserGetAuthToken(TestAdminUsername, TestAdminSecret, TestSessionID)
+	assert.NoError(t, err)
+
+	var token *oauth2.Token
+	err = json.NewDecoder(resp.Body).Decode(&token)
+	assert.NoError(t, err)
+	assert.Equal(t, usr.AuthToken, token)
+
+	assert.Empty(t, token.RefreshToken)
+	assert.NotEmpty(t, token.AccessToken)
+}
+
+func Test_UserGetAuthToken_NotAuthorized(t *testing.T) {
+	dropDB()
+	setupDB()
+
+	resp, err := UserGetAuthToken(TestUserName, TestUserSecret, TestSessionID)
+	assert.NoError(t, err)
+
+	var response handlers.FrontendError
+	err = json.NewDecoder(resp.Body).Decode(&response)
+	assert.NoError(t, err)
+
+	assert.Equal(t, handlers.SpotifyNotAuthenticated, response)
+}
