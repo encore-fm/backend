3f097bf4fd964047a66db3224df7d740353d8ae7
Anton Baumann <antonbaumann4@gmail.com>
2020-04-15 15:31:45 +0200

    add systest for skip

---
+++ systest/player_skip_test.go
@@ -0,0 +1,34 @@
+package systest
+
+import (
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPlayerSkip(t *testing.T) {
+	dropDB()
+	setupDB()
+
+	_, err := resetPlayerController(TestSessionID)
+	assert.NoError(t, err)
+
+	time.Sleep(2 * time.Second)
+
+	p, err := getPlayer()
+	assert.NoError(t, err)
+	assert.Equal(t, testSession.SongList[0], p.CurrentSong.ID)
+
+	resp, err := PlayerSkip(TestAdminUsername, TestAdminSecret, TestSessionID)
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+
+	// short pause because PlayerPlay is async
+	time.Sleep(200 * time.Millisecond)
+	p, err = getPlayer()
+	assert.NoError(t, err)
+	assert.Equal(t, testSession.SongList[1], p.CurrentSong.ID)
+}
+
