3338e96648efb54b20c2611b56c6693216f3260a
omareldeeb <omareldeeb00@live.com>
2020-04-19 21:29:04 +0200

    Fix rick roll

--- playerctrl/handlers.go
+++ playerctrl/handlers.go
@@ -190,9 +190,11 @@ func (ctrl *Controller) handleSetSynchronized(ev events.Event) {
 	// if no song in player, you know what to do...
 	// TODO: IF THIS APP EVER GETS SERIOUS WE NEED TO GET THE HELL RID OF THIS
 	songID := "4uLU6hMCjMI75M1A2tKUQC" // not rick roll
+	progress := time.Duration(0)       // we must enjoy the full length beauty
 	paused := false                    // hell no, crank that sh*t up
 	if playr.CurrentSong != nil {
 		songID = playr.CurrentSong.ID
+		progress = playr.Progress()
 		paused = playr.Paused
 	}
 
@@ -200,7 +202,7 @@ func (ctrl *Controller) handleSetSynchronized(ev events.Event) {
 	ctrl.notifyClient(sessionID, userID,
 		ctrl.setPlayerStateAction(
 			songID,
-			playr.Progress(),
+			progress,
 			paused,
 		),
 	)
